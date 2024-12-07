845808d065f64f9fb7228d43f1c01f1e80f178ec
eesitasen <sen.ee@northeastern.edu>
2024-12-07 13:10:24 -0500

    cloudwatch in server

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+const winston = require('winston');
+const WinstonCloudWatch = require('winston-cloudwatch');
+
 require('dotenv').config();
 const { app, sequelize } = require('./app'); // Importing app and sequelize
 
 const port = process.env.PORT || 8080;
+
+const cloudwatchConfig = {
+  logGroupName: process.env.CLOUDWATCH_LOG_GROUP_NAME || 'MyAppLogGroup', // Set your log group name
+  logStreamName: process.env.CLOUDWATCH_LOG_STREAM_NAME || 'MyAppLogStream', // Set your log stream name
+  region: process.env.AWS_REGION || 'us-west-2', // Set your AWS region
+};
+
+// Initialize CloudWatch service
+const cloudwatch = new AWS.CloudWatchLogs({
+  region: cloudwatchConfig.region,
+});
+
+// Set up Winston logger with CloudWatch transport
+const logger = winston.createLogger({
+  level: 'info', // Log level
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(), // Console log transport
+    new WinstonCloudWatch({
+      cloudWatchLogs: cloudwatch,
+      logGroupName: cloudwatchConfig.logGroupName,
+      logStreamName: cloudwatchConfig.logStreamName,
+      awsRegion: cloudwatchConfig.region,
+      jsonMessage: true, // Log in JSON format
+      flushInterval: 2000, // Set flush interval (in ms)
+    }),
+  ],
+});
+
+app.use((req, res, next) => {
+  logger.info({
+    message: 'Incoming request',
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+  });
+  next();
+}); 
 
 
 const startServer = async () => {
